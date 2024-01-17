ba17633b4347642cc33e7266850e406efef0a1ee
Whiskydog <alan.varde@gmail.com>
2024-01-17 12:30:08 -0300

    Solve exercise 9.3

--- section-b/bmiCalculator.ts
+++ section-b/bmiCalculator.ts
@@ -1,12 +1,43 @@
+interface HealthStats {
+  height: number,
+  weight: number
+}
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2;
 
   switch (true) {
-    case bmi < 18.5: return 'Underweight'
-    case bmi < 25: return 'Normal (healthy weight)'
-    case bmi < 30: return 'Overweight';
-    default: return 'Obese';
+    case bmi < 18.5:
+      return 'Underweight';
+    case bmi < 25:
+      return 'Normal (healthy weight)';
+    case bmi < 30:
+      return 'Overweight';
+    default:
+      return 'Obese';
   }
-}
+};
 
-console.log(calculateBmi(180, 74))
+const parseArgs = (args: string[]): HealthStats => {
+  if (args.length < 2)
+    throw new Error('Not enough arguments!');
+
+  const height = Number(args[0]);
+  const weight = Number(args[1]);
+
+  if (isNaN(height) || isNaN(weight))
+    throw new Error('You must provide numbers as arguments!');
+
+  return { height, weight };
+};
+
+const args = process.argv.slice(2);
+try {
+  const { height, weight } = parseArgs(args);
+  console.log(calculateBmi(height, weight));
+} catch (e) {
+  let message = 'There was an error.';
+  if (e instanceof Error)
+    message += ` ${e.message}`;
+  console.log(message);
+}
--- section-b/exerciseCalculator.ts
+++ section-b/exerciseCalculator.ts
@@ -10,21 +10,49 @@ interface Result {
   average: number
 }
 
+interface TrainingData {
+  period: number[],
+  target: number
+}
+
+const parseArgs = (args: string[]): TrainingData => {
+  if (args.length < 2)
+    throw new Error('You must provide target and period hours!');
+
+  const period = args.slice(1).map((arg) => Number(arg));
+  const target = Number(args[0]);
+
+  if (isNaN(target))
+    throw new Error('Target hours must be a number!');
+  period.forEach((hours) => {
+    if (isNaN(hours))
+      throw new Error('Period hours must be exclusively numbers!');
+  });
+
+  return { period, target };
+};
+
 const getRating = (average: number, target: number): Rating => {
   switch (true) {
-    case average >= target: return 3;
-    case average >= target / 2: return 2;
-    default: return 1;
+    case average >= target:
+      return 3;
+    case average >= target / 2:
+      return 2;
+    default:
+      return 1;
   }
-}
+};
 
 const getRatingDescription = (rating: Rating): string => {
   switch (rating) {
-    case 1: return `You'll have to put in more hours..`
-    case 2: return `You can do better than that...`
-    case 3: return `Great job! You've achieved your target!`
+    case 1:
+      return `You'll have to put in more hours..`;
+    case 2:
+      return `You can do better than that...`;
+    case 3:
+      return `Great job! You've achieved your target!`;
   }
-}
+};
 
 const calculateExercises = (period: number[], target: number): Result => {
   const periodLength = period.length;
@@ -43,10 +71,15 @@ const calculateExercises = (period: number[], target: number): Result => {
     target,
     average
   };
+};
+
+const args = process.argv.slice(2);
+try {
+  const { period, target } = parseArgs(args);
+  console.log(calculateExercises(period, target));
+} catch (e) {
+  let message = 'There was an error.';
+  if (e instanceof Error)
+    message += ` ${e.message}`;
+  console.log(message);
 }
-
-const trainingPeriod = [3, 0, 2, 4.5, 0, 3, 1];
-const targetHours = 2;
-
-console.log(calculateExercises(trainingPeriod, targetHours));
-

946ba20d8bcd0bc27093efc9e80d7bce7cca47f0
Whiskydog <alan.varde@gmail.com>
2024-01-20 10:55:52 -0300

    Solve exercise 9.7

---
+++ section-b/exerciseHelper.ts
@@ -0,0 +1,76 @@
+type Rating = 1 | 2 | 3;
+
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: Rating;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+interface TrainingData {
+  period: number[];
+  target: number;
+}
+
+export const parseArgs = (args: string[]): TrainingData => {
+  if (args.length < 2)
+    throw new Error('You must provide target and period hours!');
+
+  const period = args.slice(1).map((arg) => Number(arg));
+  const target = Number(args[0]);
+
+  if (isNaN(target)) throw new Error('Target hours must be a number!');
+  period.forEach((hours) => {
+    if (isNaN(hours))
+      throw new Error('Period hours must be exclusively numbers!');
+  });
+
+  return { period, target };
+};
+
+const getRating = (average: number, target: number): Rating => {
+  switch (true) {
+    case average >= target:
+      return 3;
+    case average >= target / 2:
+      return 2;
+    default:
+      return 1;
+  }
+};
+
+const getRatingDescription = (rating: Rating): string => {
+  switch (rating) {
+    case 1:
+      return `You'll have to put in more hours..`;
+    case 2:
+      return `You can do better than that...`;
+    case 3:
+      return `Great job! You've achieved your target!`;
+  }
+};
+
+export const calculateExercises = (
+  period: number[],
+  target: number
+): Result => {
+  const periodLength = period.length;
+  const trainingDays = period.filter((value) => value > 0).length;
+  const average = period.reduce((acc, value) => acc + value, 0) / periodLength;
+  const success = average >= target;
+  const rating = getRating(average, target);
+  const ratingDescription = getRatingDescription(rating);
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
--- section-b/index.ts
+++ section-b/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import { calculateBmi, parseArgs } from './bmiHelper';
+import { calculateExercises } from './exerciseHelper';
 
 const app = express();
 app.use(morgan('tiny'));
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -18,6 +20,27 @@ app.get('/bmi', (req, res) => {
   } catch {
     res.status(400).send({ error: 'Malformed parameters' });
   }
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const period: any = req.body.daily_exercises;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const target: any = req.body.target;
+
+  if (typeof period === 'undefined' || typeof target === 'undefined') {
+    return res.status(400).send({ error: 'Parameters missing' });
+  }
+
+  if (
+    Array.isArray(period) &&
+    period.every((value) => typeof value === 'number') &&
+    typeof target === 'number'
+  ) {
+    return res.send(calculateExercises(period as number[], target));
+  }
+
+  return res.status(400).send({ error: 'Malformed parameters' });
 });
 
 const PORT = 3000;

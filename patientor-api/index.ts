a234b128fd56fd4e4fd5e39e8d7483b7ee7b60ea
Whiskydog <alan.varde@gmail.com>
2024-02-02 01:31:00 -0300

    Solve exercise 9.9

--- patientor-api/index.ts
+++ patientor-api/index.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 app.use(morgan('tiny'));
+app.use(cors());
 app.use(express.json());
 
-app.get('/ping', (_req: Request, res: Response) => {
+app.get('/api/ping', (_req: Request, res: Response) => {
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
